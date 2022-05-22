1090549645a3aa3f2c3e91aa9feb266e61bfe1e0
wahi90 <mdshahed1098@gmail.com>
2022-05-22 11:18:26 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navber from './Navber';
+
+const Home = () => {
+    return (
+        <>
+        
+         
+        <div className='  lg:px-14'>
+            <Navber/>
+        </div>
+        
+        </>
+      
+    );
+};
+
+export default Home;
